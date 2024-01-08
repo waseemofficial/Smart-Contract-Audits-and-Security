762dbbab3bb0e71bfa4c8207aa0741512e5c642a
was <sy.waseemofficial@gmail.com>
2024-01-08 12:18:59 +0530

    $Foundty NFT 1

---
+++ foundry-NFT/src/BasicNft.sol
@@ -0,0 +1,10 @@
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.18;
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract BasicNft {
+    /**
+     *@note: constructing a Basic Simple NFT
+     *@dev: use OpenZeppelin/openzeppelin-contracts
+     */
+}
